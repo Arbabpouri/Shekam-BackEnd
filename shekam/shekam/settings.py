d6d8c99b80db455ec46bb939564cb2e7d705d283
MohammadMahdi Arbabpouri <mohammadmahdiarbabpouri@gmail.com>
2024-11-22 11:24:43 +0330

    add user_app

--- shekam/shekam/settings.py
+++ shekam/shekam/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restframework',
     'reservation_app',
+    'user_app',
 ]
 
 MIDDLEWARE = [

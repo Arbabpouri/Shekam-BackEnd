3cc1e4dc4bb2cfa27ea8ef7620d6c3a9c99d6cc8
MohammadMahdi Arbabpouri <mohammadmahdiarbabpouri@gmail.com>
2025-02-08 13:00:23 +0330

    update user model

--- shekam/user_app/models.py
+++ shekam/user_app/models.py
@@ -2,8 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
-class User(AbstractUser):
 
+class CustomUser(AbstractUser):
 
-    pass
+    class Meta:
+        db_table = 'users'
+        verbose_name = "کاربر"
+        verbose_name_plural = "کاربران"
 
